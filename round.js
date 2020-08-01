103a6dc9bbe996e96bd7ebffaad6635ff6225f3c
Andrew Grathwohl <andrew@grathwohl.me>
2020-08-01 15:11:58 -0400

    more metadata for Question class extension

--- round.js
+++ round.js
@@ -8,6 +8,15 @@ class Question extends Round {
   constructor(opts={}) {
     super(opts)
     this.type = 'question'
+
+    if (!opts.text || !opts.seconds) {
+      throw new Error('opts.text and opts.seconds must be provided')
+    }
+
+    this.text = opts.text
+    this.seconds = opts.seconds
+
+    this.points = opts.points ? Number(opts.points) : 1
   }
 }
 
