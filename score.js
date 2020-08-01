dd5e494a2771923d762c846d6a8a44d526602489
Andrew Grathwohl <andrew@grathwohl.me>
2020-08-01 15:04:35 -0400

    add a `Participant` to a `Scoreboard`

--- score.js
+++ score.js
@@ -1,6 +1,15 @@
+const Participant = require('./participant')
+
 class Scoreboard {
   constructor(opts={}) {
+    this.participants = []
     this.opts = opts
+  }
+
+  addParticipant(participant) {
+    if (participant instanceof Participant) {
+      this.participants.push(participant)
+    }
   }
 }
 
